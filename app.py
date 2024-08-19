27be357af63bf490a59c2b14d779a4e203a4be08
James Hirst <jameshirst@microsoft.com>
2024-08-19 10:58:27 +0100

    Make entire app more basic

--- app.py
+++ app.py
@@ -7,15 +7,5 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-# @app.route('/run_simulation', methods=['POST'])
-# def run_simulation():
-#     hyperparameters = request.json
-#     # Extract hyperparameters from the request
-#     param1 = hyperparameters.get('param1')
-#     param2 = hyperparameters.get('param2')
-#     # Run the simulation with the provided hyperparameters
-#     result = simulation(param1, param2)
-#     return jsonify(result)
-
 if __name__ == '__main__':
     app.run(debug=True)
